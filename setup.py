fcc7b801c5818fc14bceaeedac24e6848ea2f6b8
layolu layole <layolulayole@gmail.com>
2020-09-22 03:25:13 +0900

    fix for pip install

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyDoujinshiInfo',
+    packages=['pyDoujinshiInfo'],
     version='0.1',
     author='layolu layole',
     author_email='layolulayole@gmail.com',
     url='https://github.com/layolu/pyDoujinshiInfo',
-    description='API wrapper for doujinshi.info',
+    description='Unofficial API wrapper for doujinshi.info',
+    keywords='doujinshi, api-wrapper, rest-api-wrapper, python3',
     install_requires=requirements,
     license='Apache 2.0'
 )
